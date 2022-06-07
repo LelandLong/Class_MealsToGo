91a4f8011aa34d2d11807746df1d453d0d5d8dd7
LelandLong <lelandlong@me.com>
2022-06-07 08:10:40 -0400

    begin flatList

--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
-import { StatusBar, StyleSheet, SafeAreaView, Text, View } from "react-native";
+import { StatusBar, FlatList } from "react-native";
 import styled from "styled-components/native";
 import { Searchbar } from "react-native-paper";
 import { RestaurantInfoCard } from "../../components/restaurant-info-card.component";
+import { RestaurantCard } from "../../components/restaurant-info-card.styles";
+import { keyExtractor } from "react-native/Libraries/Lists/VirtualizeUtils";
 
 const SafeArea = styled.SafeAreaView`
   flex: 1;
@@ -12,12 +14,6 @@ const SafeArea = styled.SafeAreaView`
 const SearchContainer = styled.View`
   padding: ${(props) => props.theme.space[3]};
   background-color: ${(props) => props.theme.colors.bg.primary};
-`;
-
-const RestaurantListContainer = styled.View`
-  flex: 1;
-  padding: ${(props) => props.theme.space[3]};
-  background-color: ${(props) => props.theme.colors.ui.primary};
 `;
 
 export const RestaurantsScreen = () => {
@@ -32,9 +28,21 @@ export const RestaurantsScreen = () => {
           value={searchQuery}
         />
       </SearchContainer>
-      <RestaurantListContainer>
-        <RestaurantInfoCard />
-      </RestaurantListContainer>
+      <FlatList
+        data={[
+          { name: 1 },
+          { name: 2 },
+          { name: 3 },
+          { name: 4 },
+          { name: 5 },
+          { name: 6 },
+          { name: 7 },
+          { name: 8 },
+        ]}
+        renderItem={() => <RestaurantInfoCard />}
+        keyExtractor={(item) => item.name}
+        contentContainerStyle={{ padding: 16 }}
+      />
     </SafeArea>
   );
 };
